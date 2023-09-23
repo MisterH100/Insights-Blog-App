9ae6190da13f1924c2dc7d0d47ea57c4a1fe98cf
misterh100 <handsomenyathi1@gmail.com>
2023-09-23 20:24:55 +0200

    to deploy 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-"use client";
 import DropDown from './components/Dropdown';
 import Header from './components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
-import { useState} from 'react'
+
 
 export const metadata: Metadata = {
   title: 'Insights blog',
@@ -11,13 +10,12 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({children,}: {children: React.ReactNode}) {
-  const [theme, setTheme] = useState(false);
 
   return (
-    <html lang="en" data-theme={theme? "winter": "night"}>
+    <html lang="en" data-theme={"night"}>
       <body className="bg-base-100">
         <Header
-          children={<DropDown PageTheme={setTheme} theme={theme}/>}/>
+          children={<DropDown/>}/>
         {children}
       </body>
     </html>
