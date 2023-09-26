20d8c6fd66542e7e4e165e8b409a9398bdc89113
misterh100 <handsomenyathi1@gmail.com>
2023-09-26 19:53:27 +0200

    add logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import SearchBox from "./SearchBox";
+import Image from "next/image";
+import insightsLogo from "../assets/insightslogo.svg";
 
 const Header =()=>{
     return(
         <header className="w-full shadow-xl drop-shadow-lg bg-base-100 text-base-content">
             <div className="flex flex-col md:flex-row navbar h-32">
                 <div className="flex-1 font-title">
-                    <Link href={'/'} className="btn btn-ghost normal-case text-5xl">insights</Link>
+                    <Link href={'/'} className="normal-case text-5xl">
+                       <Image
+                            src={insightsLogo}
+                            alt="Insghts Logo"
+                       />    
+                    </Link>
                 </div>
                 <div className="w-full md:w-auto flex justify-between md:flex-none gap-2">
                    <SearchBox/>
